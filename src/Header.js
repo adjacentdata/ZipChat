bc0ad3baaee384b5af50dcfe72a4a841b1c3817a
albert3rd <atpcodeio@gmail.com>
2021-03-06 15:25:30 +1000

    Added Styled Components

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Header_Style = styled.div`
+    color: red;
+`;
+
+const Left = styled.div`
+
+`
+
+const Header_Avatar = styled.div`
+
+`
+
+function Header() {
+    return (
+        <Header_Style>
+            <h1>T</h1>
+        </Header_Style>
+    )
+}
+
+export default Header
+
+
+
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   text-align: center;
+//   color: palevioletred;
+// `;
