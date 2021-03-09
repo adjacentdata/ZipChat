40e3d463d92ca1de3742ebbc96f5f14ff2d60f64
albert3rd <atpcodeio@gmail.com>
2021-03-10 04:19:48 +1000

    Sidebar Edits

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
 import styled from 'styled-components'
 
 const SidebarWrapper = styled.div`
@@ -8,20 +9,38 @@ const SidebarWrapper = styled.div`
     display: flex;
     flex: .2;
     border: black solid 2px;
-    margin-top: 60px;
+    margin-top: 70px;
+    color: white;
 
 `
-const SidebarUserHeader = styled.div``
-const SidebarUserInfo = styled.div``
+const SidebarUserHeader = styled.div`
+    display: flex;
+    padding: 10px;
+
+    > .MuiSvgIcon-root{
+        border-radius: 999px;
+        font-size: 50px;
+        background-color: white;
+        color: grey;
+        padding: 10px
+    }
+`
+const SidebarUserInfo = styled.div`
+    > h4 > .MuiSvgIcon-root{
+        color: #39ff14;
+        text-shadow: 2px 2px 2px black;
+    }
+`
 
 function Sidebar() {
     return (
         <SidebarWrapper>
             <SidebarUserHeader>
                 <SidebarUserInfo>
-                    <h3>TreeBees</h3>
+                    <h3>Scrotcherino</h3>
                     <h4><FiberManualRecordIcon/>Scott Ishizu</h4>
                 </SidebarUserInfo>
+                <BorderColorIcon/>
             </SidebarUserHeader>
         </SidebarWrapper>
     )
