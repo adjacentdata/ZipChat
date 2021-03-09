1b0b98512d444385b514c673220649d3b64ce16e
albert3rd <atpcodeio@gmail.com>
2021-03-09 18:44:49 +1000

    Document Formatting

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from "./Header"
+import { styled } from '@material-ui/core';
+
+// Style
+
+const AppWrapper = styled.div`
+
+`
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Header/>
-          </Route>
-        </Switch>
-    </Router>
-
+        <AppWrapper>
+          <Header/>
+          <Switch>
+            <Route path="/" exact></Route>
+          </Switch>
+        </AppWrapper>
+      </Router>
     </div>
   );
 }
-
 export default App;
