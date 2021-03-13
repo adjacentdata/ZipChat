b9fa095790b5ee5af68336354480420ca573949b
albert3rd <atpcodeio@gmail.com>
2021-03-13 19:28:11 +1000

    Created Messages.js

---
+++ src/Messages.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MessagesWrapper = styled.div`
+
+`
+
+function Messages() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Messages
