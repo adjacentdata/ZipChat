6fc76187354ea4951deea6f407d23c261ef2f1eb
albert3rd <atpcodeio@gmail.com>
2021-03-14 02:05:23 +1000

    FrontEnd designing

--- src/InputBox.js
+++ src/InputBox.js
@@ -1,11 +1,28 @@
+import { Button, TextField } from '@material-ui/core'
 import React from 'react'
 import styled from 'styled-components'
+import SendIcon from '@material-ui/icons/Send';
 
-function InputBox() {
+const InputBoxWrapper = styled.div``
+
+function InputBox(channelId, channelName) {
     return (
-        <div>
-
-        </div>
+        <InputBoxWrapper>
+            <form>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Enter message here"
+                    variant='outlined'
+                    multiline
+                    rows={1}
+                    defaultValue="Default Value"
+                    style={{width: "90%"}}
+                />
+                <Button>
+                    <SendIcon/>
+                </Button>
+            </form>
+        </InputBoxWrapper>
     )
 }
 
