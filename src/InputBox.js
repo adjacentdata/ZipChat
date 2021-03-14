8d8e2008c357639fbd49d14e84be4484560b53ce
albert3rd <atpcodeio@gmail.com>
2021-03-14 15:52:26 +1000

    Formatting front end

--- src/InputBox.js
+++ src/InputBox.js
@@ -1,27 +1,53 @@
-import { Button, TextField } from '@material-ui/core'
-import React from 'react'
+import { Button} from '@material-ui/core'
+import {React, useRef} from 'react'
 import styled from 'styled-components'
 import SendIcon from '@material-ui/icons/Send';
+import {database} from './firebase_console'
+import firebase from 'firebase'
+const InputBoxWrapper = styled.div`
+    border-radius: 2px;
+    > form {
+        position: relative;
+        display: flex;
+        justify-content: center;
+    }
 
-const InputBoxWrapper = styled.div``
+    >form > input {
+        border: 1px solid lightblue;
+        bottom: 20px;
+        position: fixed;
+    }
+
+`
 
 function InputBox(channelId, channelName) {
+    const messageRef = useRef(null);
 
     const handleMessageSent = e => {
         e.preventDefault();
+
+        if (channelId) {
+            return false;
+        }
+
+        database.collection('Channel').doc(channelId).collection('messages').add({
+            message: messageRef.current.value,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+
+
     }
 
     return (
         <InputBoxWrapper>
             <form>
-                <TextField
+                <input
                     id="outlined-multiline-static"
                     label="Enter message here"
                     variant='outlined'
                     multiline
                     rows={1}
-                    defaultValue="Default Value"
-                    style={{width: "90%"}}
+                    style={{width: "90%" }}
                 />
                 <Button hidden type='submit' onClick={handleMessageSent}>
                     <SendIcon/>
